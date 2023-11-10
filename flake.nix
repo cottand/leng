1d1c956c2e38a5e71a734ea25ebccdbf30bc263f
Cottand <nico.dc@outlook.com>
2023-11-10 12:12:32 +0000

    simply flake, update README

--- flake.nix
+++ flake.nix
@@ -6,43 +6,41 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    let
-      build = { system, vendorSha256 }:
-        let pkgs = import nixpkgs { inherit system; }; in pkgs.buildGo121Module {
-          inherit vendorSha256;
-          pname = "grimd";
-          version = "0.0.1-test";
-          src = ./.;
-        };
-    in
     (flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in rec {
+      let pkgs = import nixpkgs { inherit system; }; in {
 
-        ## Build & packaging
-        packages.grimd = build {
-          inherit system;
-          vendorSha256 = "sha256-5dIZzqaw88lKuh1JHJurRZCPgrNzDHK/53bXKNGQBvQ=";
-        };
-
-        defaultPackage = packages.grimd;
-
-
-        ## Dev environment 
-        devShells = rec {
-          grimd = pkgs.mkShell {
-            packages = [ pkgs.fish pkgs.go_1_21 ];
-            # Note that `shellHook` still uses bash syntax. This starts fish, then exists the bash shell when fish exits.
-            shellHook = ''
-              fish && exit
-            '';
+        # Build & packaging
+        ## use with `nix build`
+        packages = rec {
+          grimd = pkgs.buildGo121Module {
+            inherit system;
+            vendorSha256 = "sha256-5dIZzqaw88lKuh1JHJurRZCPgrNzDHK/53bXKNGQBvQ=";
+            pname = "grimd";
+            version = "0.0.1-test";
+            src = ./.;
           };
           default = grimd;
         };
 
-        ## App
+
+        # Dev environment
+        ## use with `nix develop`
+        devShells = rec {
+          grimd = pkgs.mkShell {
+            packages = [ pkgs.fish pkgs.go_1_21 ];
+            # Note that `shellHook` still uses bash syntax. This starts fish, then exists the bash shell when fish exits.
+            shellHook = "fish && exit";
+          };
+          default = grimd;
+        };
+
+
+        # App
+        ## use with `nix run`
         apps = rec {
           grimd = flake-utils.lib.mkApp { drv = self.packages.${system}.grimd; };
           default = grimd;
         };
+
       }));
 }
