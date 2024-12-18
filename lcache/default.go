f48214f34fafb137837eaa2533cdcc2ca2b8b1d7
Cottand <nico.dc@outlook.com>
2024-12-18 12:16:34 +0000

    generify lcache

---
+++ lcache/default.go
@@ -0,0 +1,18 @@
+package lcache
+
+import "github.com/miekg/dns"
+
+var _ Entry = DefaultEntry{}
+
+// DefaultEntry is the default implementation of Entry
+type DefaultEntry struct {
+	dns.Msg
+}
+
+func (dnsEntry DefaultEntry) RRs() []dns.RR {
+	return dnsEntry.Answer
+}
+
+func NewDefault(maxSize int64) Cache[DefaultEntry] {
+	return NewGeneric[DefaultEntry](maxSize)
+}
