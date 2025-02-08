8463a4d7893917ff1d345b67ae81fa3ff9ae5335
Nico D'Cotta <45274424+cottand@users.noreply.github.com>
2025-02-08 18:04:59 +0100

    chore: update all dependencies (#84)
    - **flake deps update**
    - **go deps update**

--- vendor/github.com/bytedance/sonic/ast/api_compat.go
+++ vendor/github.com/bytedance/sonic/ast/api_compat.go
@@ -1,4 +1,4 @@
-// +build !amd64,!arm64 go1.23 !go1.16 arm64,!go1.20
+// +build !amd64,!arm64 go1.24 !go1.17 arm64,!go1.20
 
 /*
 * Copyright 2022 ByteDance Inc.
@@ -27,14 +27,14 @@ import (
 )
 
 func init() {
-    println("WARNING:(ast) sonic only supports Go1.16~1.22, but your environment is not suitable")
+    println("WARNING:(ast) sonic only supports go1.17~1.23, but your environment is not suitable")
 }
 
 func quote(buf *[]byte, val string) {
     quoteString(buf, val)
 }
 
-// unquote unescapes a internal JSON string (it doesn't count quotas at the begining and end)
+// unquote unescapes an internal JSON string (it doesn't count quotas at the beginning and end)
 func unquote(src string) (string, types.ParsingError) {
     sp := rt.IndexChar(src, -1)
     out, ok := unquoteBytes(rt.BytesFrom(sp, len(src)+2, len(src)+2))
